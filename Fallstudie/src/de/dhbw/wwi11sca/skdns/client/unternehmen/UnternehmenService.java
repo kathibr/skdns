69882ccb74ef8a7eaaa3eaa45434c34cf5b18d57
Kathi <kathibr1706@googlemail.com>
2012-10-09 17:38:26 +0200

    formatierung

--- Fallstudie/src/de/dhbw/wwi11sca/skdns/client/unternehmen/UnternehmenService.java
+++ Fallstudie/src/de/dhbw/wwi11sca/skdns/client/unternehmen/UnternehmenService.java
@@ -1,9 +1,7 @@
 package de.dhbw.wwi11sca.skdns.client.unternehmen;
 
 import java.util.List;
-
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
 import de.dhbw.wwi11sca.skdns.shared.EigenesUnternehmen;
 import de.dhbw.wwi11sca.skdns.shared.Unternehmen;
 
@@ -11,7 +9,6 @@ import de.dhbw.wwi11sca.skdns.shared.Unternehmen;
 public interface UnternehmenService {
 
 	List<Unternehmen> getUnternehmen();
-
 	EigenesUnternehmen getEigenesUnternehmen();
 
 }
--- Fallstudie/src/de/dhbw/wwi11sca/skdns/client/unternehmen/UnternehmenServiceAsync.java
+++ Fallstudie/src/de/dhbw/wwi11sca/skdns/client/unternehmen/UnternehmenServiceAsync.java
@@ -1,16 +1,13 @@
 package de.dhbw.wwi11sca.skdns.client.unternehmen;
 
 import java.util.List;
-
 import com.google.gwt.user.client.rpc.AsyncCallback;
-
 import de.dhbw.wwi11sca.skdns.shared.EigenesUnternehmen;
 import de.dhbw.wwi11sca.skdns.shared.Unternehmen;
 
 public interface UnternehmenServiceAsync {
 	
 	void getUnternehmen(AsyncCallback<List<Unternehmen>> callback);
-
 	void getEigenesUnternehmen(AsyncCallback<EigenesUnternehmen> callback);
 
 }
