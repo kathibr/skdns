cd701a7c88a369d2b37c4d6e5ef8e6af6813734b
Steven <Steven@Programmierlapi>
2012-10-29 21:51:05 +0100

    Debugging MarketSimulation

--- Fallstudie/src/de/dhbw/wwi11sca/skdns/server/MarketSimulation.java
+++ Fallstudie/src/de/dhbw/wwi11sca/skdns/server/MarketSimulation.java
@@ -32,11 +32,11 @@ public class MarketSimulation {
 			1.02, 1.02, 1.01, 1.00, 0.99, 0.98, 1.01, 1.05, 1.02, 0.99, 1.03 };
 
 	// Schritt 3
-	final static double marketingRandom[] = { 0.00, 0.00, 0.00, 0.00, 0.05,
-			0.05, 0.05, 0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.15, 0.15, 0.2 };
+	final static double marketingRandom[] = { 1.00, 1.00, 1.00, 1.00, 1.05,
+			1.05, 1.05, 1.05, 1.05, 1.05, 1.1, 1.1, 1.1, 1.15, 1.15, 1.2 };
 
 	// Schritt 5
-	int gewinn = 0;
+	int amount = 0;
 	int amortization = 0;
 	String unusedMachineCapacityInfo;
 
@@ -56,44 +56,48 @@ public class MarketSimulation {
 
 		// Schritt 1
 		// Berechnung des Gesamtumsatz des Marktes
-		
-		//habs einfach mal gleichgesetzt um dafür nen wert zu haben .. wenn du "topLineMarketArithmetic =  " entfernst ist alles wie vorher
-		topLineMarketArithmetic =  calculateTopLineMarket(topLineMarketArithmetic,
+		// Umsatz aller Unternehmen addieren
+
+		topLineMarket = calculateTopLineMarket(topLineMarket,
 				ownCompany.getTopLine(), company1.getTopLine(),
 				company2.getTopLine(), company3.getTopLine());
 
 		// Schritt 2
 		// Ermittlung des Marktwachstums und erneute Berechnung des Gesamtumsatz
 		// des Markets
+		// Wachstum des Markets bestimmen (in Abhängigkeit des Umsatzes)
 		increaseRandom(1, 16, random);
 		topLineMarket = (int) Math.ceil(increaseRandom[random] * topLineMarket);
 
 		// Schritt 3
 		// Ermittlung des Umsatz aller Unternehmen
-		calculateTopLineOwnCompany(version.getMarketing(),
-				ownCompany.getTopLine(), ownCompany.getProduct().getPrice(),
-				version.getPrice());
+		// Marketingmaßnahmen und Preis werden berücksichtigt
+		// bei den Unternehmen Berücksichtigung durch Randomwerte
+		ownCompany.setTopLine(calculateTopLineOwnCompany(
+				version.getMarketing(), ownCompany.getTopLine(), ownCompany
+						.getProduct().getPrice(), version.getPrice()));
 
-		calculateTopLineCompanies(company1.getTopLine());
-		calculateTopLineCompanies(company2.getTopLine());
-		calculateTopLineCompanies(company3.getTopLine());
+		company1.setTopLine(calculateTopLineCompanies(company1));
+		company2.setTopLine(calculateTopLineCompanies(company2));
+		company3.setTopLine(calculateTopLineCompanies(company3));
 
 		// Schritt 4
 		// Ermittlung des Marktgesamtumsatzes anhand der in Schritt 3
 		// ermittelten Unternehmensumsätze
-		calculateTopLineMarket(topLineMarket, ownCompany.getTopLine(),
+		topLineMarketArithmetic = calculateTopLineMarket(
+				topLineMarketArithmetic, ownCompany.getTopLine(),
 				company1.getTopLine(), company2.getTopLine(),
 				company3.getTopLine());
 
 		// Ermittlung der Unternehmensmarktanteile
-		calculateMarketShare(topLineMarketArithmetic, ownCompany.getTopLine(),
-				ownCompany.getMarketShare());
-		calculateMarketShare(topLineMarketArithmetic, company1.getTopLine(),
-				company1.getMarketShare());
-		calculateMarketShare(topLineMarketArithmetic, company2.getTopLine(),
-				company2.getMarketShare());
-		calculateMarketShare(topLineMarketArithmetic, company3.getTopLine(),
-				company3.getMarketShare());
+		ownCompany.setMarketShare(calculateMarketShare(topLineMarketArithmetic,
+				ownCompany.getTopLine(), ownCompany.getMarketShare()));
+		company1.setMarketShare(calculateMarketShare(topLineMarketArithmetic,
+				company1.getTopLine(), company1.getMarketShare()));
+		company2.setMarketShare(calculateMarketShare(topLineMarketArithmetic,
+				company2.getTopLine(), company2.getMarketShare()));
+		company3.setMarketShare(calculateMarketShare(topLineMarketArithmetic,
+				company3.getTopLine(), company3.getMarketShare()));
 
 		// Schritt 5
 		// Ermittlung der Unternehmensabsatzmengen
@@ -122,21 +126,39 @@ public class MarketSimulation {
 			version.setMarketIncrease(0);
 		} // Ende if-else
 
+		// Anzahl der neu eingestellten Mitarbeiter wird auf die
+		// Mitarbeiteranzahl des eigenen Unternehmens gerechnet
+		ownCompany.setNumberOfStaff(ownCompany.getNumberOfStaff()
+				+ version.getPersonal());
+		// Die Werte der neu gekauften Maschine werden auf die Werte der
+		// Maschine des eigenen Unternehmens gerechnet
+		ownCompany.getMachines().setCapacity(
+				version.getMachineCapacity()
+						+ ownCompany.getMachines().getCapacity());
+		ownCompany.getMachines()
+				.setStaff(
+						version.getMachineStaff()
+								+ ownCompany.getMachines().getStaff());
+		ownCompany.getMachines().setAccountingValue(
+				version.getMachineValue()
+						+ ownCompany.getMachines().getAccountingValue());
+
+		// Sonderfall 2
+		// Ermittelt, ob die Kapazitäten der Maschinen ausgelastet sind
+		specialCaseTwo(version, ownCompany);
+
 		// Ermittlung der Maschinenabschreibungen
-		getAmortization(amortization, version.getMachineValue(),
+		amortization = getAmortization(amortization, version.getMachineValue(),
 				ownCompany.getMachines());
 
+		// Schritt 6 Ausgaben berechnen
 		// Ermittlung des Gewinns des eigenen Unternehmens
-		gewinn = (int) Math.ceil(ownCompany.getTopLine()
+		amount = (int) Math.ceil(ownCompany.getTopLine()
 				- ownCompany.getFixedCosts() - ownCompany.getSalaryStaff()
 				* ownCompany.getNumberOfStaff() - version.getMarketing()
 				- ownCompany.getVariableCosts() - amortization);
 
-		ownCompany.setAmount(gewinn);
-
-		// Sonderfall 2
-		// Ermittelt, ob die Kapazitäten der Maschinen ausgelastet sind
-		specialCaseTwo(version);
+		ownCompany.setAmount(amount);
 
 		return version;
 	} // Ende method simulate
@@ -155,38 +177,93 @@ public class MarketSimulation {
 			necessaryPersonalInfo = new String(
 					"Für die neue Maschine ist nicht genügend Personal vorhanden und kann somit nicht in Betrieb genommen werden. Es fehlen mindestens "
 							+ necessaryPersonal + " Mitarbeiter.");
+			System.out.println("necessaryPersonal:" + necessaryPersonal);
 		} // Ende if-Statement
 
 	} // Ende method specialCaseOne
 
-	public void specialCaseTwo(SimulationVersion version) {
-		if (ownCompany.getProduct().getSalesVolume() < ownCompany.getMachines()
-				.getCapacity()) {
+	public void specialCaseTwo(SimulationVersion version, OwnCompany ownCompany) {
+
+		// Absatzmenge des eigenen Unternehmens berechnen
+		int salesVolume = calculateSalesVolume(ownCompany.getTopLine(),
+				ownCompany.getProduct().getPrice());
+
+		if (ownCompany.getMachines().getCapacity() < salesVolume) {
+			// Die Absatzmenge entspricht der 100% Maschinenauslastung
+			ownCompany.getProduct().setSalesVolume(
+					ownCompany.getMachines().getCapacity());
+			// Der Umsatz sinkt
+			ownCompany.setTopLine((int) Math.ceil(salesVolume
+					* ownCompany.getProduct().getPrice()));
+			System.out.println("Kapazität: "
+					+ ownCompany.getMachines().getCapacity());
 			unusedMachineCapacityInfo = new String(
-					"Die Kapazitäten sind nicht voll ausgelastet. Möchten Sie noch eine Maschine kaufen? Sie könnten dadurch Ihr Betriebergebnis steigern!");
-			double MarketSharesCompanies = company1.getMarketShare()
+					"Möchten Sie noch eine Maschine kaufen? Sie könnten dadurch Ihr Betriebergebnis steigern!");
+			// Marktanteil der Konkurrenzunternehmen
+			double marketSharesCompanies = company1.getMarketShare()
 					+ company2.getMarketShare() + company3.getMarketShare();
-			calculateAdditionalVolume(MarketSharesCompanies, company1);
-			calculateAdditionalVolume(MarketSharesCompanies, company2);
-			calculateAdditionalVolume(MarketSharesCompanies, company3);
-		} // Ende if-Statement
+			// Umrechnung der Überschussabsatzmenge auf die
+			// Konkurrenzunternehmen
+			calculateAdditionalVolume(marketSharesCompanies, company1);
+			calculateAdditionalVolume(marketSharesCompanies, company2);
+			calculateAdditionalVolume(marketSharesCompanies, company3);
+
+			// Berechnen des neuen Gesamtmarktes
+			topLineMarket = calculateTopLineMarket(topLineMarket,
+					ownCompany.getTopLine(), company1.getTopLine(),
+					company2.getTopLine(), company3.getTopLine());
+			// neue Marktanteile berechen
+			// Ermittlung der Unternehmensmarktanteile
+			ownCompany.setMarketShare(calculateMarketShare(
+					topLineMarketArithmetic, ownCompany.getTopLine(),
+					ownCompany.getMarketShare()));
+			company1.setMarketShare(calculateMarketShare(
+					topLineMarketArithmetic, company1.getTopLine(),
+					company1.getMarketShare()));
+			company2.setMarketShare(calculateMarketShare(
+					topLineMarketArithmetic, company2.getTopLine(),
+					company2.getMarketShare()));
+			company3.setMarketShare(calculateMarketShare(
+					topLineMarketArithmetic, company3.getTopLine(),
+					company3.getMarketShare()));
+
+		} else {
+			ownCompany.getProduct().setSalesVolume(salesVolume);
+		} // Ende if-else
+			// Berechnen der Absatzmengen der Konkurrenzunternehmen
+		company1.getProduct().setSalesVolume(
+				calculateSalesVolume(company1.getTopLine(), company1
+						.getProduct().getPrice()));
+		company2.getProduct().setSalesVolume(
+				calculateSalesVolume(company2.getTopLine(), company2
+						.getProduct().getPrice()));
+		company3.getProduct().setSalesVolume(
+				calculateSalesVolume(company3.getTopLine(), company3
+						.getProduct().getPrice()));
 	} // Ende method specialCaseTwo
+
+	private int calculateSalesVolume(int topLine, double price) {
+		double topLineCalc = topLine;
+		int salesVolume = (int) Math.ceil(topLineCalc / price);
+		return salesVolume;
+	}
 
 	private Company calculateAdditionalVolume(double marketSharesCompanies,
 			Company company) {
 		double distributionKey = company.getMarketShare()
 				/ marketSharesCompanies;
-
 		double salesVolume = (ownCompany.getProduct().getSalesVolume() - ownCompany
 				.getMachines().getCapacity())
 				* ownCompany.getProduct().getPrice()
 				* distributionKey
 				+ company.getProduct().getSalesVolume();
 
-		company.setTopLine((int) (salesVolume * company.getProduct().getPrice()));
+		company.getProduct().setSalesVolume((int) Math.ceil(salesVolume));
 
-		calculateMarketShare(topLineMarketArithmetic, company.getTopLine(),
-				company.getMarketShare());
+		System.out.println("SalesVolume: " + salesVolume);
+
+		// neuer Umsatz des Unternehmens
+		company.setTopLine((int) (salesVolume * company.getProduct().getPrice()));
 		return company;
 
 	} // Ende method calculateAdditionalVolume
@@ -194,23 +271,28 @@ public class MarketSimulation {
 	public static int calculateTopLineMarket(int topLineMarket,
 			int tLOwnCompany, int tLCompany1, int tLCompany2, int tLCompany3) {
 
-		return topLineMarket = tLOwnCompany + tLCompany1 + tLCompany2
-				+ tLCompany3;
+		topLineMarket = tLOwnCompany + tLCompany1 + tLCompany2 + tLCompany3;
+		return topLineMarket;
 	} // Ende method calculateTopLineMarket
 
 	public static int increaseRandom(int low, int high, int random) {
-		return random = (int) Math.round(Math.random() * (high - low) + low);
-
+		random = (int) Math.round(Math.random() * (high - low) + low);
+		return random;
 	} // Ende method increaseRandom
 
-	public static int calculateTopLineCompanies(int topLine) {
+	public static int calculateTopLineCompanies(Company company) {
+		int topLine = company.getTopLine();
 
-		// Marketing berechnen
-		int random = (int) Math.random() * 15 + 1;
+		// Marketing berechnen (anhand Random)
+		int random = (int) Math.ceil(Math.random() * 14 + 1);
 		double marketing = marketingRandom[random];
 
-		// Preisfaktor berechnen
+		// Preisfaktor berechnen (anhand Random)
 		double priceFactor = Math.random() * (1.01 - 0.99) + 0.99;
+
+		// neuer Preis des Unternehmens ermitteln
+		company.getProduct().setPrice(
+				priceFactor / company.getProduct().getPrice());
 
 		// Umsatz berechnen
 		topLine = (int) Math.ceil(topLine * marketing * priceFactor);
@@ -220,33 +302,46 @@ public class MarketSimulation {
 	public static int calculateTopLineOwnCompany(int marketingInput,
 			int topLine, double priceOld, double priceNew) {
 
-		double marketing = marketingInput / topLine;
+		double marketingInputCalc = marketingInput;
+		double topLineCalc = topLine;
+		// Werbeumsatzanstieg
+		double marketing = marketingInputCalc / topLineCalc;
 
 		if (marketing > 0.2) {
-			marketing = 0.2;
-		} // Ende if-Statement
+			marketing = 1.2;
+		} else {
+			marketing = marketing + 1.0;// Ende if-Statement
+		}
+		// Preisänderung
+		if (priceNew == 0) {
+			priceNew = priceOld;
+		}
 		double priceFactor = priceOld / priceNew;
-
 		topLine = (int) (Math.ceil(topLine * marketing * priceFactor));
 		return topLine;
 	} // Ende method calculateTopLineOwnCompany
 
 	public static double calculateMarketShare(int topLine, int topLineCompany,
 			double marketShare) {
-		return marketShare = (marketShare + (topLineCompany / topLine * 100)) * 0.5;
+		double topLineCompanyCalc = topLineCompany;
+		double topLineCalc = topLine;
+		marketShare = (marketShare + (topLineCompanyCalc / topLineCalc) * 100) * 0.5;
+		return marketShare;
 	} // Ende method calculateMarketShare
 
 	public static int calculateSalesVolume(int topLineMarket,
 			double marketShare, double price, int salesVolume) {
 		int realTopLine = (int) (topLineMarket * marketShare / 100);
 
-		return salesVolume = (int) Math.ceil(realTopLine / price);
+		salesVolume = (int) Math.ceil(realTopLine / price);
+		return salesVolume;
 	} // Ende method calculateSalesVolume
 
 	private static int getAmortization(int amortization, int machineValue,
 			Machines machine) {
 
-		return amortization = (int) Math.ceil(machineValue / 10
+		amortization = (int) Math.ceil(machineValue / 10
 				+ machine.getAccountingValue() / machine.getServiceLife());
+		return amortization;
 	} // Ende method getAmortization
 } // Ende class MarketSimulation
