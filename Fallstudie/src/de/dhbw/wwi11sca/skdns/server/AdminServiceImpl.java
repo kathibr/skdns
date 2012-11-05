21e71dafdfe48367cf9389b2effcb665685345d1
Kathi <D056867@WDFN00255782A>
2012-11-05 13:31:01 +0100

    adminserviceimpl

--- Fallstudie/src/de/dhbw/wwi11sca/skdns/server/AdminServiceImpl.java
+++ Fallstudie/src/de/dhbw/wwi11sca/skdns/server/AdminServiceImpl.java
@@ -14,7 +14,10 @@ import com.mongodb.MongoException;
 import de.dhbw.wwi11sca.skdns.client.admin.AdminService;
 import de.dhbw.wwi11sca.skdns.shared.Admin;
 import de.dhbw.wwi11sca.skdns.shared.Company;
+import de.dhbw.wwi11sca.skdns.shared.Machines;
 import de.dhbw.wwi11sca.skdns.shared.OwnCompany;
+import de.dhbw.wwi11sca.skdns.shared.Product;
+import de.dhbw.wwi11sca.skdns.shared.SimulationVersion;
 import de.dhbw.wwi11sca.skdns.shared.User;
 
 public class AdminServiceImpl extends RemoteServiceServlet implements
@@ -48,26 +51,41 @@ public class AdminServiceImpl extends RemoteServiceServlet implements
 	@Override
 	public void saveUser(User newUser) {
 		newUser.setUserID(newUser.getUsername());		
-	
+		
 		OwnCompany ownCompany = new OwnCompany();
+		Product ownProduct = new Product();
+		Machines ownMachines = new Machines();
+		ownMachines.setUserID(newUser.getUsername());
+		ownCompany.setMachines(ownMachines);
+		ownProduct.setUserID(newUser.getUsername());
+		ownCompany.setProduct(ownProduct);
 		ownCompany.setTradeName("Eigenes Unternehmen");
 		ownCompany.setUserID(newUser.getUsername());
+		ownCompany.setCompanyID("ownCom");
 		
 		Company company1 = new Company();
 		company1.setTradeName("Competitior");
 		company1.setUserID(newUser.getUsername());
 		company1.setCompanyID("1");
+		Product productCom1 = new Product();
+		productCom1.setUserID(newUser.getUsername());
+		company1.setProduct(productCom1);
 		
 		Company company2 = new Company();
 		company2.setTradeName("Competitior");
 		company2.setUserID(newUser.getUsername());
 		company2.setCompanyID("2");
+		Product productCom2 = new Product();
+		productCom2.setUserID(newUser.getUsername());
+		company2.setProduct(productCom2);
 		
 		Company company3 = new Company();
 		company3.setTradeName("Competitior");
 		company3.setUserID(newUser.getUsername());
 		company3.setCompanyID("3");
-		
+		Product productCom3 = new Product();
+		productCom3.setUserID(newUser.getUsername());
+		company3.setProduct(productCom3);
 		
 		Datastore ds = new Morphia().createDatastore(getMongo(), "skdns");
 		ds.save(newUser);
@@ -75,6 +93,7 @@ public class AdminServiceImpl extends RemoteServiceServlet implements
 		ds.save(company1);
 		ds.save(company2);
 		ds.save(company3);
+		
 		
 		
 	} // Ende method saveUser
@@ -85,7 +104,9 @@ public class AdminServiceImpl extends RemoteServiceServlet implements
 		ds.delete(ds.createQuery(User.class).filter("userID = ", deleteUser));
 		ds.delete(ds.createQuery(OwnCompany.class).filter("userID = ", deleteUser));
 		ds.delete(ds.createQuery(Company.class).filter("userID = ", deleteUser));
-		
+		ds.delete(ds.createQuery(Product.class).filter("userID = ", deleteUser));
+		ds.delete(ds.createQuery(Machines.class).filter("userID = ", deleteUser));
+		ds.delete(ds.createQuery(SimulationVersion.class).filter("userID = ", deleteUser));
 	} // Ende method deleteUser
 
 	@Override
